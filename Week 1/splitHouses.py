c55b008d80f86a7e82893cec62faedf370eaf7bb
yashjsimform <yash.j@simformsolutions.com>
2022-01-04 17:19:16 +0530

    splitHouse

---
+++ Week 1/splitHouses.py
@@ -0,0 +1,25 @@
+t = int(input())
+c = input()
+s=[]
+
+if t > 1:
+    if "." not in c:
+        print('NO')
+    else:
+        if "HH" not in c:
+            print("YES")
+            c = c.replace(".", "B")
+            print(c)
+        else:
+            print('NO')
+else:
+    if c == "H" or c == ".":
+        print("YES")
+        print(c)
+    elif c == ".":
+        print("YES")
+        c = c.replace(".", "B")
+        print(c)
+    else:
+        print("NO")
+
